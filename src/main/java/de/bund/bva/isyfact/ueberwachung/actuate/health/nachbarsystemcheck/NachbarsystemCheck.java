571f608fa80707912ce874e934788d9f2b174582
Christian Dirks <christian.dirks@materna.group>
2026-09-02 11:56:51 +0200

    docs: IFS-5758 add deprecation note for Nachbarsystemchecks

--- src/main/java/de/bund/bva/isyfact/ueberwachung/actuate/health/nachbarsystemcheck/NachbarsystemCheck.java
+++ src/main/java/de/bund/bva/isyfact/ueberwachung/actuate/health/nachbarsystemcheck/NachbarsystemCheck.java
@@ -4,18 +4,20 @@ import de.bund.bva.isyfact.ueberwachung.actuate.health.nachbarsystemcheck.model.
 import de.bund.bva.isyfact.ueberwachung.actuate.health.nachbarsystemcheck.model.NachbarsystemHealth;
 
 /**
- * Health-Überprüfung eines Nachbarsystems.
+ * Health check of a neighbouring system.
+ * @deprecated Deprecated since IF6
  */
 @FunctionalInterface
+@Deprecated
 public interface NachbarsystemCheck {
 
     /**
-     * Health-Überprüfung eines Nachbarsystems.
-     * Der Health-Endpoint des Nachbarsystems wird abgefragt
-     * und das Ergebnis in ein NachbarsystemHealth-Objekt gekapselt.
+     * Health check of a neighbouring system.
+     * The health endpoint of the neighbour is queried and the
+     * output is capsuled into a NachbarsystemHealth-object.
      *
-     * @param nachbarsystem das zu überprüfende Nachbarsystem
-     * @return Ergebnis der Überprüfung
+     * @param nachbarsystem neighbouring system
+     * @return return value of the health check
      */
     NachbarsystemHealth checkNachbarsystem(Nachbarsystem nachbarsystem);
 
