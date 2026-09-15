474ab7ef9c30934746bf1f1e681988c44b4705a4
Marvin Krause <marvin.krause@materna.group>
2026-09-15 09:17:21 +0200

    feat: IFS-5784 interpret non-default isAlive path as absolute system file path

--- src/main/java/de/bund/bva/isyfact/ueberwachung/service/loadbalancer/LoadbalancerServlet.java
+++ src/main/java/de/bund/bva/isyfact/ueberwachung/service/loadbalancer/LoadbalancerServlet.java
@@ -51,16 +51,12 @@ public class LoadbalancerServlet extends HttpServlet {
         LOG.info(LogKategorie.JOURNAL, EreignisSchluessel.PLUEB00001, "Initialisiere Loadbalancer-Servlet.");
 
         String isAliveFileLocation = getInitParameter(PARAM_IS_ALIVE_FILE_LOCATION);
-        if (isAliveFileLocation == null) {
-            LOG.info(LogKategorie.JOURNAL, EreignisSchluessel.PLUEB00001,
-                "Position der IsAliveDatei nicht konfiguriert. Verwende Standard-Einstellung: {}",
-                DEFAULT_IS_ALIVE_FILE_LOCATION);
-            isAliveFileLocation = DEFAULT_IS_ALIVE_FILE_LOCATION;
+        if (DEFAULT_IS_ALIVE_FILE_LOCATION.equals(isAliveFileLocation)) {
+            String realPath = getServletContext().getRealPath(isAliveFileLocation);
+            isAliveFile = new File(realPath);
+        } else {
+            isAliveFile = new File(isAliveFileLocation);
         }
-        String realPath = getServletContext().getRealPath(isAliveFileLocation);
-        isAliveFile = realPath != null
-                ? new File(realPath)
-                : new File(isAliveFileLocation);
 
         LOG.info(LogKategorie.JOURNAL, EreignisSchluessel.PLUEB00001, "IsAlive-Datei {} konfiguriert.",
             isAliveFile.getAbsolutePath());
